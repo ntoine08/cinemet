d44420f81f41d1c1ffc093c9aa533d8550122d29
[ntoin08] <[j.antoine689@laposte.net]>
2019-05-15 10:58:13 +0200

    tentative avec romain

--- insertion.php
+++ insertion.php
@@ -1,31 +1,26 @@
 <?php
-    $cnx = mysql_connect("localhost", "sunjian", "ichigo08"); // inclure la connection
+    try
+    {
+     $bdd = new PDO('mysql:host=localhost;dbname=cinemets;charset=utf8mb4', 'sunjian', 'ichigo08', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    }
+    catch(Exception $e) 
+    {
+     die('Erreur '.$e->getMessage());
+    }
 
-    //SÉLECTION DE LA BASE DE DONNÉES
-    $db = mysql_select_db("cinemets");
-
-    //récupération des valeurs de champs
+    /*récupération des valeurs de champs
     $nom_film = $_POST["nom_film"];
     $date_de_sortie_film = $_POST["date_de_sortie_film"];
     $duree_film = $_POST["duree_film"];
     $img_film = $_POST["img_film"];
     $nationalite_film = $_POST["nationalite_film"];
-    $synopsis_film = $_POST["synopsis_film"];
+    $synopsis_film = $_POST["synopsis_film"];*/
 
     //création de la requête sql
-    $sql = "INSERT INTO films (nom_film, date_de_sortie_film, duree_film, img_film, nationalite_film, synopsis_film) VALUES ('$nom_film', '$date_de_sortie_film', '$duree_film', '$img_film', '$nationalite_film', '$synopsis_film')";
+    $sql = "INSERT INTO films (nom_film, date_de_sortie_film, duree_film, img_film, nationalite_film, synopsis_film) VALUES ('fff', '2019-04-10', '01h 41min', 'img/test.jpg', 'fr', 'franchement c'est trop bien comme film en pdo !')";
 
-    //execution de la requête
-    $requete = mysql_query($sql, $cnx) or die(mysql_error());
+    $bdd -> exec ($sql);
+    echo "c'est super, c'est génial";
 
-    //affichage des résultats, pour savoir si ça a marché
-    if($requete)
-    {
-        echo("insertion correcte");
-    }
-    else
-    {
-        echo("insertion echec");
-    }
-
+    
 ?>
